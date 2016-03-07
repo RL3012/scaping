dc55e48162c643355cdc570632bbe9afd6463173
riverluo <wenjingluo2015@gmail.com>
2016-03-07 23:14:03 +0800

    fix

--- crawler.py
+++ crawler.py
@@ -1,33 +1,30 @@
+# -*- coding: utf-8 -*-
+
 import urllib
 
 def get_page(url):
-    # input as url and output as a string which is
-    # the text of that page
-    content=urllib.urlopen(seed_page).read()
+    content=urllib.urlopen(url).read()
     return content
 
 
 def find_next_url(content):
-    # a string as input,output the first url  and
-    # end position of the url's closing quote
-    start_pos=content.find("href",content.find("<a"))
-    if start_pos==-1:
-        return None,"no links found"
-    first_quote=content.find('''"''',start_pos)
+    start_link=content.find("<a")
+    if start_link==-1:
+        return None,0
+    start_href=content.find("href",start_link)
+    first_quote=content.find('''"''',start_href)
     end_quote=content.find('''"''',first_quote+1)
     url=content[first_quote+1:end_quote]
     return url,end_quote
 
 
-
 def get_all_links(content):
-    #input as string content and output as
-    #all the links in a list
     links=[]
     while True:
         url,end_quote=find_next_url(content)
-        if url:
-            links.append(url)
+        if url is not None:
+            if len(url)>0:
+                links.append(url)
             content=content[end_quote:]
         else:
             break
@@ -48,13 +45,14 @@ def crawl_web(seed_page):
         if url not in crawled:
             crawled.append(url)
             union(tocrawl,get_all_links(get_page(url)))
-            # tocrawl.extend(get_all_links(get_page(url)))
     return crawled
 
-seed_page="http://xkcd.com/"
-# seed_page="https://www.udacity.com/cs101x/index.html"
-# print get_all_links(get_page(seed_page))
+# seed_page="http://xkcd.com/"
+seed_page="https://www.udacity.com/cs101x/index.html"
 print crawl_web(seed_page)
+
+
+
 
 
 
